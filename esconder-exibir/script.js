b39a8664bf046f72b333d11265b0e8193e6d89e9
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-04-20 12:03:16 -0300

    função esconder

---
+++ esconder-exibir/script.js
@@ -0,0 +1,10 @@
+function esconde(el){
+    document.getElementById(el).style.display = 'none';
+}
+
+/*
+el= elemento
+chamando a função esconde
+procura no documento o elemento com id EL, modificando seu display no style para none
+*/
+
