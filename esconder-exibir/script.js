3ceeef5440d3d435c1b28b68e0d40191ebd22d10
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-04-20 12:54:44 -0300

    função toggle

--- esconder-exibir/script.js
+++ esconder-exibir/script.js
@@ -18,3 +18,24 @@ chamando a função mostra
 procura no documento o elemento com id EL, modificando seu display no style para block (Exibir)
 */
 
+function toggle(el){
+    var display = document.getElementById(el).style.display;
+    if(display == "block"){
+    document.getElementById(el).style.display = 'none';
+    }
+    else{
+        document.getElementById(el).style.display = 'block';
+    }
+
+
+}
+
+/*
+el= elemento
+chamando a função toggle
+pega qual é o valor da propriedade display
+analisa qual o valor atual para fazer a alteração.
+
+caso minha display esteja exibindo ela tem que sumir,
+caso contrário, ela tem que aparecer
+*/
